1d6558640b614ebeb52c034850dbd5029130b61b
rivercrown <heguanbin1997@qq.com>
2018-08-05 12:31:34 +0800

    add a new constructor and a setter func for startStamp

--- SnowFlake.java
+++ SnowFlake.java
@@ -10,43 +10,102 @@ public class SnowFlake {
     /**
      * 起始的时间戳
      */
-    private final static long START_STMP = 1480166465631L;
+    private long startStamp;
 
     /**
      * 每一部分占用的位数
      */
-    private final static long SEQUENCE_BIT = 12; //序列号占用的位数
-    private final static long MACHINE_BIT = 5;   //机器标识占用的位数
-    private final static long DATACENTER_BIT = 5;//数据中心占用的位数
+//    private final long SEQUENCE_BIT; //序列号占用的位数
+//    private final long MACHINE_BIT;   //机器标识占用的位数
+//    private final long DATACENTER_BIT;//数据中心占用的位数
 
     /**
      * 每一部分的最大值
      */
-    private final static long MAX_DATACENTER_NUM = -1L ^ (-1L << DATACENTER_BIT);
-    private final static long MAX_MACHINE_NUM = -1L ^ (-1L << MACHINE_BIT);
-    private final static long MAX_SEQUENCE = -1L ^ (-1L << SEQUENCE_BIT);
+    private final long MAX_DATACENTER_NUM;
+    private final long MAX_MACHINE_NUM;
+    private final long MAX_SEQUENCE;
 
     /**
      * 每一部分向左的位移
      */
-    private final static long MACHINE_LEFT = SEQUENCE_BIT;
-    private final static long DATACENTER_LEFT = SEQUENCE_BIT + MACHINE_BIT;
-    private final static long TIMESTMP_LEFT = DATACENTER_LEFT + DATACENTER_BIT;
+    private final long MACHINE_LEFT;
+    private final long DATACENTER_LEFT;
+    private final long TIMESTMP_LEFT;
 
-    private long datacenterId;  //数据中心
+    //每一部分的默认值
+    private final static long SEQ_BIT_DEFAULT = 12;
+    private final static long MACHINE_BIT_DEFAULT = 5;
+    private final static long DATACENTER_BIT_DEFAULT = 5;
+
+    //每一部分的最少位数和最大位数
+    private final static int MIN_BIT = 1;
+    private final static int MAX_BIT = 60;
+
+    private final static int TOTAL_BIT = 63;//位数总和最大值
+
+    private long dataCenterId;  //数据中心
     private long machineId;     //机器标识
     private long sequence = 0L; //序列号
     private long lastStmp = -1L;//上一次时间戳
 
-    public SnowFlake(long datacenterId, long machineId) {
-        if (datacenterId > MAX_DATACENTER_NUM || datacenterId < 0) {
-            throw new IllegalArgumentException("datacenterId can't be greater than MAX_DATACENTER_NUM or less than 0");
+    public SnowFlake(long dataCenterId, long machineId) {
+        this(System.currentTimeMillis(), SEQ_BIT_DEFAULT, MACHINE_BIT_DEFAULT, DATACENTER_BIT_DEFAULT, 
+                dataCenterId, machineId);
+    }
+    public SnowFlake(long startStamp, long sequenceBit, long machineBit, long dataCenterBit,
+                     long dataCenterId, long machineId) {
+        //时间戳配置
+        this.startStamp = startStamp;
+
+        //计算最大值
+        long[] nums = {sequenceBit, machineBit, dataCenterBit};
+        validateBit(nums);
+        MAX_DATACENTER_NUM = -1L ^ (-1L << dataCenterBit);
+        MAX_MACHINE_NUM = -1L ^ (-1L << machineBit);
+        MAX_SEQUENCE = -1L ^ (-1L << sequenceBit);
+
+        //计算左移位数
+        MACHINE_LEFT = sequenceBit;
+        DATACENTER_LEFT = MACHINE_LEFT + machineBit;
+        TIMESTMP_LEFT = DATACENTER_LEFT + dataCenterBit;
+
+        //判断 dataCenterId 和 machineId 的准确性
+        if (dataCenterId > MAX_DATACENTER_NUM || dataCenterId < 0) {
+            throw new IllegalArgumentException("dataCenterId can't be greater than MAX_DATACENTER_NUM or less than 0");
         }
         if (machineId > MAX_MACHINE_NUM || machineId < 0) {
             throw new IllegalArgumentException("machineId can't be greater than MAX_MACHINE_NUM or less than 0");
         }
-        this.datacenterId = datacenterId;
+        this.dataCenterId = dataCenterId;
         this.machineId = machineId;
+    }
+
+    private static void validateBit(long[] nums) {
+        long totalBitCount = 0;
+        for (long num : nums) {
+            //判断各个部分的位数是否在合法范围内
+            if (num < MIN_BIT || num > MAX_BIT)
+                throw new IllegalArgumentException("bit out of range (should between [" + MIN_BIT +
+                        ", " + MAX_BIT + "])");
+            else
+                totalBitCount += num;
+        }
+        //判断总位数是否合法
+        if (totalBitCount > TOTAL_BIT - MIN_BIT)
+            throw new IllegalArgumentException("total bit is too large (should be smaller than " + (TOTAL_BIT - MIN_BIT) + " )");
+    }
+
+    private long getNextMill() {
+        long mill = getNewstmp();
+        while (mill <= lastStmp) {
+            mill = getNewstmp();
+        }
+        return mill;
+    }
+
+    private long getNewstmp() {
+        return System.currentTimeMillis();
     }
 
     /**
@@ -74,30 +133,31 @@ public class SnowFlake {
 
         lastStmp = currStmp;
 
-        return (currStmp - START_STMP) << TIMESTMP_LEFT //时间戳部分
-                | datacenterId << DATACENTER_LEFT       //数据中心部分
+        return (currStmp - startStamp) << TIMESTMP_LEFT //时间戳部分
+                | dataCenterId << DATACENTER_LEFT       //数据中心部分
                 | machineId << MACHINE_LEFT             //机器标识部分
                 | sequence;                             //序列号部分
     }
 
-    private long getNextMill() {
-        long mill = getNewstmp();
-        while (mill <= lastStmp) {
-            mill = getNewstmp();
-        }
-        return mill;
-    }
-
-    private long getNewstmp() {
-        return System.currentTimeMillis();
+//    设置开始时间戳，用于刷新 ID
+//    注意修改了 startStamp 之后就会生成重复的 ID
+//    只有当该算法生成的 ID 只是序列号的一部分时（如序列号由 (日期 + ID) 组成时）
+//    才应该考虑使用 set 重置开始时间戳
+    public void setStartStamp(long startStamp) {
+        this.startStamp = startStamp;
     }
 
     public static void main(String[] args) {
-        SnowFlake snowFlake = new SnowFlake(2, 3);
+        SnowFlake snowFlake = new SnowFlake(System.currentTimeMillis(), 11, 2, 2, 0, 0);
 
-        for (int i = 0; i < (1 << 12); i++) {
+        for (int i = 0; i < 20; i++) {
             System.out.println(snowFlake.nextId());
         }
 
+        snowFlake.setStartStamp(System.currentTimeMillis());
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println(snowFlake.nextId());
+        }
     }
 }
